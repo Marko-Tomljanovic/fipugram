f2c5a5e99b80335212de1f133f200c5606ded401
Marko Tomljanovic <markot@windowslive.com>
2021-01-08 17:05:03 +0100

    Update fipugram

---
+++ src/views/Singup.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="about">
+      <div class="mx-auto" style="width: 200px;">    
+    <div class="align-middle"> <br><br>
+<form>
+  <div class="form-group">
+    <label for="exampleFullName">Full Name</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+    <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="form-group">
+    <label for="repeatPassword"> Repeat Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+  <label class="form-check-label" for="exampleRadios1">Male
+  </label>
+</div>
+    </div>
+    <div class="col-sm">
+      <div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+  <label class="form-check-label" for="exampleRadios2">Female
+  </label>
+</div>
+    </div>
+  </div>
+</div><br><br>
+
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">I accept and understand the "Software engineering" class</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
+  </div>
+</template>
+
+
+
+<style>
+  @import './../assets/bootstrap.css';
+</style>
