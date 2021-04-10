dc07099c9b0f4394647a83a67f597edd33c83cd0
Marko Tomljanovic <markot@windowslive.com>
2021-04-10 18:24:59 +0200

    Firebase 1
    + instalacija firebase
    + kreiranje javascript file firebase
    + varijable di se upisuju imputi
    + singup funkcije u methotds koja nam sluzi za registraciju korisnika

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD2MjPBcSxtSwRRkq3Kn9CP0tty9zZ8Nmo",
+    authDomain: "fipugram-4a32a.firebaseapp.com",
+    projectId: "fipugram-4a32a",
+    storageBucket: "fipugram-4a32a.appspot.com",
+    messagingSenderId: "445593751717",
+    appId: "1:445593751717:web:dd8cd93fad75c75023bd4c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export {
+      firebase,
+  }
--- src/views/Singup.vue
+++ src/views/Singup.vue
@@ -5,20 +5,20 @@
 <form>
   <div class="form-group">
     <label for="exampleFullName">Full Name</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input v-model="fullname" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
     <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label for="exampleInputEmail1">Username</label>
+    <input v-model="username" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
   </div>
   <div class="form-group">
-    <label for="repeatPassword"> Repeat Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="repeatPassword">Repeat Password</label>
+    <input v-model="repeatpassword" type="password" class="form-control" id="exampleInputPassword1">
   </div>
 
 <div class="container">
@@ -44,9 +44,40 @@
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">I accept and understand the terms</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="button" @click="singup" class="btn btn-primary">Registracija</button>
 </form>
 </div>
 </div>
   </div>
 </template>
+
+<script>
+import {firebase} from '@/firebase.js'
+
+export default {
+  name: "SingnUp",
+  data(){
+    return{
+      fullname:'',
+      username:'',
+      password:'',
+      repeatpassword:'',
+    };
+  },
+  methods: {
+    singup(){
+      firebase.auth().createUserWithEmailAndPassword(this.username, this.password).then(
+        function(){
+          console.log('Usiješna registracija');
+        }
+      )
+      .catch(function(error){
+        console.error('Došlo je do greške', error);
+        alert('Password mora minimalno 6 znakova!!');
+      })
+      console.log('Nastavak');
+          }
+
+  }
+}
+</script>
